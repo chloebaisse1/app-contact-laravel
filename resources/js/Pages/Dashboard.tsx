f338a030b61f4ad038c73e4dbfb26f0064bb8472
chloebaisse1 <baissechloe@gmail.com>
2025-03-17 17:37:59 +0400

    ajout nouveau contact sur le dahsboard

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,29 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import { Contact, PropsDashboard } from "@/types";
+import { useState } from "react";
+import { Button } from "@/Components/ui/button";
+import { Plus } from "lucide-react";
 
-export default function Dashboard() {
+export default function Dashboard({ contacts }: PropsDashboard) {
+    const [showModal, setShowModal] = useState(false);
+    const [selectedContact, setSelectedContact] = useState<
+        Contact | undefined
+    >();
+    const [modalType, setModalType] = useState<"create" | "edit">("create");
+
+    const handleCreate = () => {
+        setModalType("create");
+        setSelectedContact(undefined);
+        setShowModal(true);
+    };
+
+    const handleEdit = (contact: Contact) => {
+        setModalType("edit");
+        setSelectedContact(contact);
+        setShowModal(true);
+    };
+
     return (
         <AuthenticatedLayout
             header={
@@ -15,8 +37,15 @@ export default function Dashboard() {
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
-                            You're logged in!
+                        <div className="p-6">
+                            <h2 className="text-2xl font-semibold text-gray-900">
+                                Mes contacts
+                            </h2>
+
+                            <Button onClick={handleCreate} className="mt-4">
+                                <Plus className="w-5 h-5" />
+                                Nouveau contact
+                            </Button>
                         </div>
                     </div>
                 </div>
